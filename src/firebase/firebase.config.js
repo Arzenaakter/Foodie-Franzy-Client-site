6c5535a9f60ebcfd72646ea9c8e220a318d62aa3
Arzena Akter <arzenaakter@gmail.com>
2023-05-04 02:42:10 +0600

    Use the Environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwKF7kYGjftNQ0NOS6cqaiyLHn_IExM6g",
-  authDomain: "foodie-franzy.firebaseapp.com",
-  projectId: "foodie-franzy",
-  storageBucket: "foodie-franzy.appspot.com",
-  messagingSenderId: "549234430985",
-  appId: "1:549234430985:web:354ddf65fbe4ab301e14d8"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
