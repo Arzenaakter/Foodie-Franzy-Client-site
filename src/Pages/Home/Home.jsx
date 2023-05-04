6a6093b1a89e8cdd198bbff07e129db13338db63
Arzena Akter <arzenaakter@gmail.com>
2023-05-04 15:41:01 +0600

    add menu section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Cards from '../Cards/Cards';
 import ClientReview from '../ClientReview/ClientReview';
+import Menu from '../Menu/Menu/Menu';
 
 const Home = () => {
     return (
        <>
         <Banner></Banner>
         <Cards></Cards>
+        <Menu></Menu>
         <ClientReview></ClientReview>
        </>
     );
---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import MenuItem from '../MenuItem/MenuItem';
+
+
+const Menu = () => {
+  const[recipes,setRecipes]=  useState([])
+    useEffect(() => { 
+        fetch('http://localhost:5000/recipes')
+        .then(res =>res.json())
+        .then(data=>setRecipes(data))
+     }, [])
+
+
+
+    return (
+        <div className='bg-base-100 mt-10'>
+                <h2 className="text-3xl font-bold text-center border-b-2 w-72 mx-auto pt-10 pb-2  mb-10">
+                      Our Menus
+                  </h2>
+
+
+        <div className=''>
+            {
+                recipes.map(recipe =><MenuItem
+                key={recipe.id}
+                recipe={recipe}
+                ></MenuItem>)
+
+            }
+
+        </div>
+            
+        </div>
+    );
+};
+
+export default Menu;
